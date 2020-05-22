6be6efe268b584073a1de46b17caa3f02f6fb987
Jonathan Buchanan <johnyburd8@gmail.com>
2020-05-22 05:25:02 -0400

    stream

--- api.py
+++ api.py
@@ -1,33 +1,38 @@
 from flask import Flask, request
 from pathlib import Path
+from io import BytesIO
 import tarfile
 
 app = Flask(__name__)
 
-@app.route('/upload', methods=['POST'])
+
+@app.route('/api/upload', methods=['POST'])
 def upload():
-    if 'build' in request.files:
-        tarball = request.files['build']
-        event = request.args.get('event')
-        event_id = request.args.get('event_id')
-        if not (event and event == 'pr' or event == 'commit'):
-            return {'message': "missing type"}, 400
-        if not event_id:
-            return {'message': "missing event_id"}, 400
+    event = request.args.get('event')
+    event_id = request.args.get('event_id')
+    if not (event and event == 'pr' or event == 'commit'):
+        return {'message': "missing type"}, 400
+    if not event_id:
+        return {'message': "missing event_id"}, 400
 
-        print(tarball.filename)
-        #with open(f'www/{build.filename}', 'w') as f:
-        try:
-            build = tarfile.open(fileobj=tarball.stream)
-        except tarfile.ReadError:
-            return {'message': "not a tarfile"}, 400
-            #f.write(build.stream.read().decode())
+    tarball = BytesIO()
+    while True:
+        chunk = request.stream.read(4096)
+        if len(chunk) == 0:
+            tarball.seek(0)
+            break
+        tarball.write(chunk)
 
-        Path(f"www/{event}/{event_id}").mkdir(parents=True, exist_ok=True)
-        build.extractall(f"www/{event}/{event_id}")
-        print(build.list())
-        return {'url': f"https://staging.archit.us/{event}/{event_id}"}, 200
-    return {'message': "missing file"}, 400
+    try:
+        build = tarfile.open(fileobj=tarball)
+    except tarfile.ReadError as e:
+        print(e)
+        return {'message': "not a tarfile"}, 400
+
+    Path(f"www/{event}/{event_id}").mkdir(parents=True, exist_ok=True)
+    build.extractall(f"www/{event}/{event_id}")
+    return {'url': f"https://staging.archit.us/{event}/{event_id}"}, 200
+
 
 if __name__ == '__main__':
-        app.run()
+    app.run(port=5454)
